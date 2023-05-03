54dcb1e1175617e978d790372f841f4d3c462ddd
shohagnawaz <snawazdigital@gmail.com>
2023-05-03 16:51:23 +0600

    client

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import AppHero from './components/Hero';
 import AppAbout from './components/About';
 import AppService from './components/Services';
 import AppWorks from './components/Works';
+import AppTeams from './components/Teams';
+import AppTestimonials from './components/Testimonials';
+import AppPricing from './components/Pricing';
 
 function App() {
   return (
@@ -17,6 +20,9 @@ function App() {
         <AppAbout />
         <AppService />
         <AppWorks />
+        <AppTeams />
+        <AppTestimonials />
+        <AppPricing />
       </main>
     </div>
   );
---
+++ src/components/Pricing.js
@@ -0,0 +1,19 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+export default function AppPricing() {
+    return (
+        <section id='pricing' className='block pricing-block'>
+            <Container fluid>
+                <div className='title-holder'>
+                    <h2>PRICING & PLANS</h2>
+                    <div className='subtitle'>
+                        Check Our Pricing & Plans
+                    </div>
+
+                </div>
+            </Container>
+        </section>
+    )
+}
